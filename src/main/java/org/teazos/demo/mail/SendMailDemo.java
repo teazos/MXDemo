bfd93afa91ff7170ba73f2473596ce167df17681
teazos <teazos@163.com>
2016-05-06 18:34:47 +0800

    提交

--- src/main/java/org/teazos/demo/mail/SendMailDemo.java
+++ src/main/java/org/teazos/demo/mail/SendMailDemo.java
@@ -1,14 +1,8 @@
 package org.teazos.demo.mail;
 
-/**
- * Created by mijunfeng on 16/5/6.
- */
 import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
 
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
@@ -26,7 +20,7 @@ public class SendMailDemo {
         // 发送服务器需要身份验证
         props.setProperty("mail.smtp.auth", "true");
         // 设置邮件服务器主机名
-        props.setProperty("mail.host", "smtp.163.com");
+        props.setProperty("mail.host", "[发件服务器]");
         // 发送邮件协议名称
         props.setProperty("mail.transport.protocol", "smtp");
 
@@ -39,15 +33,15 @@ public class SendMailDemo {
             msg.setSubject("JavaMail测试");
 
             // 设置邮件内容
-            msg.setText("这是一封由<b>JavaMail</b>发送的邮件！");
+            msg.setText("这是一封由JavaMail发送的邮件！");
             // 设置发件人
-            msg.setFrom(new InternetAddress("teazos@163.com"));
+            msg.setFrom(new InternetAddress("[发件人邮箱]"));
 
             Transport transport = session.getTransport();
             // 连接邮件服务器
-            transport.connect("teazos@163.com", "mijunfeng0");
+            transport.connect("[用户名]", "[密码]");
             // 发送邮件
-            transport.sendMessage(msg, new Address[]{new InternetAddress("teazos@163.com")});
+            transport.sendMessage(msg, new Address[]{new InternetAddress("[收件人地址]")});
             // 关闭连接
             transport.close();
         } catch (javax.mail.MessagingException e) {
